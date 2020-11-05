2589ee54837fd1544166914da1d842cf34be39ba
maxxeg <maxxeg@gmail.com>
2020-11-05 18:53:44 +0400

    Initial commit

---
+++ lesson3/lesson3/SomeProject/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  SomeProject
+//
+//  Created by Nikita Petrenkov on 02/11/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        let sample = "пример"
+    print(sample)
+    
+    if false {
+        print("Будет ли выполнена эта строка?")
+        someMethod()
+    }
+}
+    
+    func someMethod() {
+        
+    }
+    
+}
+
